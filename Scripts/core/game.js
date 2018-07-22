764d055b76c2301925d5909febd9935f735a45ed
hardy91 <hardeepsng@gmail.com>
2018-07-22 19:53:24 -0400

    added reference file

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,3 +1,5 @@
+/// <reference path="_references.ts"/>
+//iffi
 (function () {
     var canvas = document.getElementById("canvas");
     var stage;
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,3 +1,6 @@
+/// <reference path="_references.ts"/>
+
+//iffi
 (function(){
 
     let canvas = document.getElementById("canvas");
