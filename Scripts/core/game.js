c90e363f11e4acac3f6a7d51de0b097503842686
hardy91 <hardeepsng@gmail.com>
2018-07-22 14:26:26 -0400

    add typescript and createjs support

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,15 +1,29 @@
-(function(){
-    function Init(){
+(function () {
+    var canvas = document.getElementById("canvas");
+    var stage;
+    var helloLabel;
+    function Init() {
         console.log("initialization Started");
+        Start();
     }
-    function Start(){
-
+    function Start() {
+        console.log("Starting Application");
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", Update);
+        Main();
     }
-    function Update(){
-
+    function Update() {
+        helloLabel.rotation -= 5;
+        stage.update();
     }
-    function Main(){
-
+    function Main() {
+        console.log("Game Starting ");
+        helloLabel = new createjs.Text("hello,world", "40px Consolas", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+        stage.addChild(helloLabel);
     }
     window.onload = Init;
 })();
+//# sourceMappingURL=game.js.map
---
+++ Scripts/core/game.ts
@@ -0,0 +1,32 @@
+(function(){
+
+    let canvas = document.getElementById("canvas");
+    let stage:createjs.Stage;
+    let helloLabel : createjs.Text;
+    function Init(){
+        console.log("initialization Started");
+        Start();
+        
+    }
+    function Start():void{
+        console.log("Starting Application")
+        stage =  new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60 ;
+        createjs.Ticker.on("tick",Update);
+        Main();
+    }
+    function Update():void{
+        helloLabel.rotation -= 5;
+        stage.update();
+    }
+    function Main():void{
+        console.log("Game Starting ");
+        helloLabel = new createjs.Text("hello,world", "40px Consolas", "#000000");
+        helloLabel.x =  100;
+        helloLabel.y =  100;
+        stage.addChild(helloLabel);
+
+
+    }
+    window.onload = Init;
+})();
