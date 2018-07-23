8a9e00adec846aa870bad31bfd484b294157eea4
hardy91 <hardeepsng@gmail.com>
2018-07-23 03:11:20 -0400

    Clened up play Scene

--- Scripts/scenes/play.js
+++ Scripts/scenes/play.js
@@ -12,32 +12,20 @@ var scenes;
 (function (scenes) {
     var PlayScene = /** @class */ (function (_super) {
         __extends(PlayScene, _super);
+        // public 
+        // private
         // constructor 
         function PlayScene(assetManager) {
             var _this = _super.call(this, assetManager) || this;
             _this.Start();
             return _this;
         }
-        PlayScene.prototype._nextButtonClick = function () {
-            objects.Game.currentScene = config.Scene.OVER;
-        };
-        PlayScene.prototype._backButtonClick = function () {
-            objects.Game.currentScene = config.Scene.START;
-        };
         PlayScene.prototype.Init = function () {
         };
         PlayScene.prototype.Start = function () {
-            this._playLabel = new objects.Label("Game playing", '40px', "Consolas", "#000000", 220, 180);
-            this._nextButton = new objects.Button(this.assetManager, "nextButton", 450, 240);
-            this._backButton = new objects.Button(this.assetManager, "backButton", 100, 240);
             this.Main();
         };
         PlayScene.prototype.Main = function () {
-            this.addChild(this._playLabel);
-            this.addChild(this._nextButton);
-            this.addChild(this._backButton);
-            this._nextButton.on("click", this._nextButtonClick);
-            this._backButton.on("click", this._backButtonClick);
         };
         PlayScene.prototype.Update = function () {
         };
